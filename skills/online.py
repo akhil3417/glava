89d5e8bc2c71b03ed8ae283151143d500ee737b8
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 10:43:17 +0530

    refactor: modify online.py to include send_email function with error handling

--- skills/online.py
+++ skills/online.py
@@ -4,3 +4,24 @@ from email.message import EmailMessage
 import smtplib
 # from decouple import config
 
+EMAIL = ""
+PASSWORD = ""
+
+def send_email(receiver_add, subject, message):
+    try:
+        email = EmailMessage()
+        email['To'] = receiver_add
+        email['Subject'] = subject
+        email['From'] = EMAIL
+
+        email.set_content(message)
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.send_message(email)
+        s.close()
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
