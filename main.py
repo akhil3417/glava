964a5f9b83f3cf9490c3a0a2d58ba451853229e6
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 11:27:08 +0530

    feat(browser): add open_in_browser func

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 from utils.memory_consumption import tell_memory_consumption
+        "browser": open_browser_command,
--- skills/browser.py
+++ skills/browser.py
@@ -3,3 +3,7 @@ import shlex
 from config import BROWSER, PROFILE, SITES
 from utils.input_output import speak_or_print, start_process,take_command
 DEFAULT_BROWSER = shlex.split(BROWSER) + shlex.split(PROFILE)  # This will also be a list of arguments
+
+def open_browser_command():
+    speak_or_print(f"Opening browser",{BROWSER})
+    start_process(DEFAULT_BROWSER)  # Unpack the list of arguments
