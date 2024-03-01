6679b8dbbf2dbdeead2897b5834e4e54055bc170
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:17:29 +0530

    refactor: utils/input_output.py - add toggle function for piper http server command

--- utils/input_output.py
+++ utils/input_output.py
@@ -2,3 +2,11 @@ import asyncio
 import logging
 import os
 import random
+def toggle_piper_http_server_command():
+    global PIPER_HTTP_SERVER
+    if PIPER_HTTP_SERVER:
+        PIPER_HTTP_SERVER = False
+        kill_piper()
+    else:
+        PIPER_HTTP_SERVER = True
+        start_piper_tts_service()  # Start the PIPER_HTTP_SERVER
