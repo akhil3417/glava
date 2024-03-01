1911e0b85a23a7af65ef6e567de4a041bd4cb3a8
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 19:04:43 +0530

    refractor(main):import necessary modules

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+#!/usr/bin/env python3
+import argparse
+import asyncio
+import os
+from datetime import datetime
+
+from pynput import keyboard
+
+# from config import VOICE_MODELS , USER , HOSTNAME
+# Import necessary modules
+from config import HOSTNAME, IS_LISTENING, USER, VOICE_MODELS, PIPER_HTTP_SERVER
+from skills.browser import (open_browser_command, open_websites,
+                            open_youtube_browser_command_async,
+                            query_web_command_async, web_search)
+from skills.info import (calculate_command, google_news_command, movie_command,
+                         read_news, weather_command, what_is_wolframe,
+                         wikipedia_command_async)
+from skills.linux import ip_address_command_async, open_new_shell_command
+from skills.music import play_song, run_stream_async
+from utils.command_handlers import term_sgpt
+from utils.services import start_vosk_service_command
 from utils.memory_consumption import tell_memory_consumption
+from utils.input_output import (generate_response, get_transcript,
+                                speak_or_print, start_listening,
+                                toggle_is_listening_command,
+                                toggle_random_voice_command,
+                                toggle_voice_command,change_voice_model_command,toggle_piper_http_server_command,toggle_vosk_websocket_server_command)
         "term": term_sgpt,
         "browser": open_browser_command,
         "wiki":wikipedia_command_async,
