8ca95dc4d37b3ad0850673145dfa44fa7e8a8daf
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 11:28:51 +0530

    feat: add wikipedia command in skills/info.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from utils.memory_consumption import tell_memory_consumption
         "browser": open_browser_command,
+        "wiki":wikipedia_command_async,
--- skills/info.py
+++ skills/info.py
@@ -1,3 +1,20 @@
 import threading
 import urllib.request
 
+import wikipedia
+def search_on_wikipedia(query):
+    try:
+        results = wikipedia.summary(query, sentences=3)
+        return results
+    except wikipedia.exceptions.PageError:
+        return "No page matches the query."
+    except wikipedia.exceptions.DisambiguationError:
+        return "There are multiple pages matching the query. Please be more specific."
+
+async def wikipedia_command_async():
+    query = await take_command()  # Await take_command() here
+    print(f"Searching on Wikipedia for",query)
+    results = search_on_wikipedia(query)
+    speak_or_print("Here's a quick summary from Wikipedia.\n")
+    speak_or_print(results)
+
