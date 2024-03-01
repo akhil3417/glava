bd4c6fdd34d8d7dd12868dc3e290d3ce5f5c629d
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 11:34:46 +0530

    feat: skills/linux.py - added functions to get and send IP addresses

--- skills/linux.py
+++ skills/linux.py
@@ -2,3 +2,22 @@ from utils.input_output import start_process,send_notification
 from config import TERMINAL
 import requests
 
+def ip_address():
+    # This is a free service that returns your IP address
+    ipv6 = requests.get('https://api64.ipify.org?format=json')
+    ip_datav6 = ipv6.json()
+    ip_addressv6 = ip_datav6['ip']
+
+    ipv4 = requests.get('https://api.ipify.org?format=json')
+    ip_datav4 = ipv4.json()
+    ip_addressv4 = ip_datav4['ip']
+
+    return ip_addressv4, ip_addressv6
+
+async def ip_address_command_async():
+    # Get the IP addresses
+    ipv4, ipv6 = ip_address()
+
+    # Send the notification
+    send_notification('IP Addresses', f'IPv4: {ipv4}\nIPv6: {ipv6}')
+
