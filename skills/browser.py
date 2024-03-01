961fb0601b42b98381ef6d57c5d2310e5778bf63
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:23:23 +0530

    nit:add spaces

--- skills/browser.py
+++ skills/browser.py
@@ -2,13 +2,17 @@ import re
 import shlex
 from config import BROWSER, PROFILE, SITES
 from utils.input_output import speak_or_print, start_process,take_command
+
+
 DEFAULT_BROWSER = shlex.split(BROWSER) + shlex.split(PROFILE)  # This will also be a list of arguments
 
 def open_browser_command():
     speak_or_print(f"Opening browser",{BROWSER})
     start_process(DEFAULT_BROWSER)  # Unpack the list of arguments
+
 def get_site_url(site_key, user_input):
     return SITES.get(site_key).format(user_input=user_input)
+
 def open_in_browser(DEFAULT_BROWSER, url):
     command = DEFAULT_BROWSER + [url]
     start_process(command)
