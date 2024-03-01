c9e3b7a5b2888baff0a6131273028459f5fd600f
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:26:39 +0530

    refactor: input_output - add speaker_ids function

--- utils/input_output.py
+++ utils/input_output.py
@@ -11,6 +11,13 @@ from config import (IS_LISTENING, IS_RANDOM_VOICE, PIPER_EXECUTABLE,
 from config import get_jarvis_prompt, get_sgpt_args,voice_model
 from .speech_recognition import start_listening
 from .services import kill_vosk, start_piper_tts_service,kill_piper
+def speaker_ids(voice_model):
+    voice_model = os.path.basename(voice_model)  # Strip the path from the voice_model value
+    if voice_model in SPEAKER_IDS:
+        return random.choice(SPEAKER_IDS[voice_model])
+    else:
+        return None
+
 def toggle_piper_http_server_command():
     global PIPER_HTTP_SERVER
     if PIPER_HTTP_SERVER:
