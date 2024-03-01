a1870204eb053ca917b72e24aaec43a26eb2ba63
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:38:32 +0530

    feat(handler):funcs to generated string>command

--- utils/command_handlers.py
+++ utils/command_handlers.py
@@ -15,3 +15,21 @@ def is_command_safe(cmd):
             return False
     return True
 
+def run_command(user_input):
+    # run_cmd = f"sgpt -s '{user_input}'"
+    run_cmd = f"sgpt --no-cache --role commandonly '{user_input}'"
+    if is_command_safe(run_cmd):
+        print(f"Requested command {run_cmd}")
+        result = subprocess.run(run_cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+        start_process(output,shell=True)
+        return output
+    else:
+        print("Unsafe command. Aborting.")
+        return None
+
+
+def term_sgpt(user_input):
+    t = threading.Thread(target=run_command, args=(user_input,))
+    t.start()
+
