520c30694c027c64ea8554ba88b5e9ee05b472db
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:45:29 +0530

    feat(skill): add movie command to fetch info from imdb , not perfect

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from utils.memory_consumption import tell_memory_consumption
         "term": term_sgpt,
         "browser": open_browser_command,
         "wiki":wikipedia_command_async,
+        # "movie": movie_command,
--- skills/info.py
+++ skills/info.py
@@ -1,6 +1,7 @@
 import threading
 import urllib.request
 
+import imdb
 import wikipedia
 def search_on_wikipedia(query):
     try:
@@ -17,4 +18,29 @@ async def wikipedia_command_async():
     results = search_on_wikipedia(query)
     speak_or_print("Here's a quick summary from Wikipedia.\n")
     speak_or_print(results)
+
+
+def movie_command():
+    movies_db = imdb.IMDb()
+    speak_or_print("Please tell me the movie name:")
+    text=input().lower()
+    movies = movies_db.search_movie(text)
+    speak_or_print("searching for" + text)
+    speak_or_print("I found these")
+    for movie in movies:
+        title = movie["title"]
+        year = movie["year"]
+        speak_or_print(f"{title}-{year}")
+        info = movie.getID()
+        movie_info = movies_db.get_movie(info)
+        rating = movie_info["rating"]
+        cast = movie_info["cast"]
+        actor = cast[0:5]
+        plot = movie_info.get('plot outline', 'plot summary not available')
+        speak_or_print(f"{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor}. "
+              f"The plot summary of movie is {plot}")
+
+        print(f"{title} was released in {year} has imdb ratings of {rating}.\n It has a cast of {actor}. \n"
+              f"The plot summary of movie is {plot}")
+
 
