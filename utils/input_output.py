aa13f396dba56a84d518279671e8932c809a65ca
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-01 18:29:13 +0530

    refactor(input_output): add speak_or_print function for voice output

--- utils/input_output.py
+++ utils/input_output.py
@@ -31,6 +31,21 @@ def get_audio_stream(json_input=False):
     command += " --sentence_silence 0.2 --noise_scale 0.333 --noise_w 0.333 --length_scale 1.3  --output-raw 2>/dev/null |  aplay -r 22050 -f S16_LE -t raw - 2>/dev/null "
     return command
 
+def speak_or_print(text, VOICE=None, json_input=False):
+    if PIPER_HTTP_SERVER:
+       command = f"curl -G --data-urlencode 'text={text}' 'localhost:5000' | aplay -r 22050 -f S16_LE -t raw - 2>/dev/null"
+       subprocess.run(command, shell=True, check=True, input=text, text=True)
+       return
+    elif VOICE is not None:
+        VOICE = global_voice
+        command = get_audio_stream(json_input)
+        try:
+            subprocess.run(command, shell=True, check=True, input=text, text=True)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error running command: {command}. Error: {e}")
+            pass
+    else:
+        print(text)
 def toggle_piper_http_server_command():
     global PIPER_HTTP_SERVER
     if PIPER_HTTP_SERVER:
