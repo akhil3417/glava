766b0d80460c5887c3bde555570de81af4ef43a1
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-03 20:39:33 +0530

    feat(config): move config to ini file and read using configparser , since this project is going upstream

--- config.py
+++ config.py
@@ -1,21 +1,50 @@
 import logging
 import os
+import configparser
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
+# Function to get config
+def get_config():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
+
+
+config = get_config()
+
+
 # User specific details
-USER = "Charlie"
-HOSTNAME = "Jarvis"
-EMAIL = ""
-PASSWORD = ""
+USER = config["USER"]["name"]
+HOSTNAME = config["USER"]["hostname"]
+EMAIL = config["USER"]["email"]
+PASSWORD = config["USER"]["password"]
 
 # API keys
-NEWS_API_KEY = ""
-OPENWEATHER_APP_ID = ""
-NEWS_HEADLINES_NUMBER = 10
-WOLFRAMALPHA_API = ""
-CITY = ""
+NEWS_API_KEY = config["API_KEYS"]["news_api_key"]
+OPENWEATHER_APP_ID = config["API_KEYS"]["openweather_app_id"]
+WOLFRAMALPHA_API = config["API_KEYS"]["wolframalpha_api"]
+
+
+# services
+IS_LISTENING = config["SERVICES"].getboolean("is_listening")
+SCREEN_PRINT = config["SERVICES"].getboolean("screen_print")
+VOICE = config["SERVICES"].getboolean("voice")
+PIPER_HTTP_SERVER = config["SERVICES"].getboolean("piper_http_server")
+VOSK_WEBSOCKET_SERVER = config["SERVICES"].getboolean("vosk_websocket_server")
+IS_RANDOM_VOICE = config["SERVICES"].getboolean("is_random_voice")
+
+# Other settings
+TERMINAL = config["OTHER_SETTINGS"]["terminal"]
+NEWS_HEADLINES_NUMBER = int(config["OTHER_SETTINGS"]["news_headlines_number"])
+CITY = config["OTHER_SETTINGS"]["city"]
+COUNTRY = config["OTHER_SETTINGS"]["country"]
+
+# browser specific
+BROWSER = "firefox"
+PROFILE = "-p KodachiBrowserLight"
 
 
 # Directories
@@ -51,31 +80,18 @@ PIPER_EXECUTABLE = os.path.join(
     os.path.abspath(os.path.join(BASE_DIR, "extensions/piper/")), "piper"
 )
 
-
-IS_RANDOM_VOICE = False
-IS_LISTENING = True
-SCREEN_PRINT = True
-
-VOICE = True
-PIPER_HTTP_SERVER = True
+# websocket scripts
 PIPER_HTTP_SERVER_SCRIPT = os.path.abspath(
     os.path.join(BASE_DIR, "scripts/piper_websocket_server.py")
 )
-
-# Piper script arguments
-PIPER_SCRIPT_ARGS = f"--sentence_silence 0.2 --noise_scale 0.333 --noise_w 0.333 --length_scale 1.3 -m {voice_model}"
-
-# VOSK model and script
-VOSK_WEBSOCKET_SERVER = False
-VOSK_WEBSOCKET_SCRIPT = os.path.join(BASE_DIR, "scripts/vosk_rest_websocket.py")
 VOSK_MODELS_DIR = os.path.join(BASE_DIR, "extensions/vosk/")
+VOSK_WEBSOCKET_SCRIPT = os.path.join(BASE_DIR, "scripts/vosk_rest_websocket.py")
 VOSK_MODEL = "vosk-model-en-in-0.5"
 
 
-# Other settings
-BROWSER = "firefox"
-PROFILE = "-p KodachiBrowserLight"
-TERMINAL = "st"
+# script arguments
+PIPER_SCRIPT_ARGS = f"--sentence_silence 0.2 --noise_scale 0.333 --noise_w 0.333 --length_scale 1.3 -m {voice_model}"
+
 
 STARTUP_MESSAGES = [
     "Cool, Im on it sir.",
@@ -96,7 +112,6 @@ def get_jarvis_prompt():
 
 
 # countries=["ae","ar","at","au","be","bg","br","ca","ch","cn","co","cu","cz","de","eg","fr","gb","gr","hk","hu","id","ie","il","in","it","jp","kr","lt","lv","ma","mx","my","ng","nl","no","nz","ph","pl","pt","ro","rs","ru","sa","se","sg","si","sk","th","tr","tw","ua","us","ve","za"]
-COUNTRY = "in"
 SITES = {
     "def_search_engine": "https://lite.duckduckgo.com/lite/?q={user_input}&kf=-1&kz=-1&kq=-1&kv=-1&k1=-1&kp=-2&kaf=1&kd=-1",
     "youtube": "http://www.youtube.com/results?aq=f&oq=&search_query={user_input}",
