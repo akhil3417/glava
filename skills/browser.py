237949cab15342562360feb545ddcdd4a25623bc
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-03 13:55:22 +0530

    nit:auto formatting

--- skills/browser.py
+++ skills/browser.py
@@ -1,7 +1,11 @@
 import re
 import shlex
 from config import BROWSER, PROFILE, SITES
-from utils.input_output import speak_or_print, start_process, take_command
+from utils.input_output import (
+    speak_or_print,
+    start_process,
+    take_command,
+)
 
 
 
