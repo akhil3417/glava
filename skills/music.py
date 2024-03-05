f2f728831ad5e8f9752e625b2836eb730aba8f19
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-05 10:38:43 +0530

    tweak(music): audio default to mpc after binary check else use mpv

--- skills/music.py
+++ skills/music.py
@@ -1,13 +1,38 @@
+import subprocess
+from shutil import which
+import re
+
+from utils.input_output import speak_or_print, take_command
+
 from .yt import get_media_url, play
-from utils.input_output import (
-    speak_or_print,
-    take_command,
-)
 
 
 def play_audio(user_input):
     media_url = get_media_url(user_input, 1)
-    play(media_url, "--ytdl-format=bestaudio --no-video")
+
+    # Extract the video ID from the URL using regex
+    video_id = re.search(r"v=(\w+)", media_url).group(1)
+
+    # check if mpc is available
+    mpc = which("mpc")
+    if mpc is not None:
+        cmd = [mpc, "add"]
+    else:
+        mpv = which("mpv")
+        if mpv is not None:
+            cmd = [mpv]
+        else:
+            raise Exception("Neither mpc nor mpv found")
+
+    cmd.append(
+        subprocess.check_output(
+            ["yt-dlp", "--prefer-insecure", "-g", "-f251", video_id]
+        )
+        .decode()
+        .strip()
+    )
+
+    subprocess.call(cmd)
 
 
 def play_video(user_input):
