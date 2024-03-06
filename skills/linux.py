4f7f095b3a396d967d1ca3e9e0856a5e10a2bad1
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-06 19:29:39 +0530

    tweak(docs): add docstrings for ip related funcs

--- skills/linux.py
+++ skills/linux.py
@@ -3,6 +3,9 @@ import requests
 
 
 def ip_address():
+    """
+    This function retrieves both IPv4 and IPv6 addresses using a free IP address service.
+    """
     # This is a free service that returns your IP address
     ipv6 = requests.get("https://api64.ipify.org?format=json")
     ip_datav6 = ipv6.json()
@@ -16,6 +19,9 @@ def ip_address():
 
 
 async def ip_address_command_async():
+    """
+    A function to asynchronously retrieve the IP addresses and send a notification.
+    """
     # Get the IP addresses
     ipv4, ipv6 = ip_address()
 
