45496307b7b85d342a7ea0ab5a8ee16504702208
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-06 19:40:15 +0530

    nit: tweak piper service starting msg

--- utils/services.py
+++ utils/services.py
@@ -15,7 +15,7 @@ def start_piper_tts_service():
     """
     command = f"python3 {PIPER_HTTP_SERVER_SCRIPT} --sentence_silence 0.2 --noise_scale 0.333 --noise_w 0.333 --length_scale 1.3 -m {voice_model}"
     print(
-        f"Starting Piper Http Server on Port 5000 with Default {os.path.basename(voice_model)} model"
+        f"Starting Piper Http Server on Port 5000 with Default '{os.path.basename(voice_model)}' voice model"
     )
     # start_process(command,shell=True)
     subprocess.Popen(
