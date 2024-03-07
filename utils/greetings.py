e78263b34bf9527f943999ae6e7ece022a98320c
akhil3417 <akhilpratapsingh3417@gmail.com>
2024-03-07 09:15:27 +0530

    refractor: add docstrings for funcs in greetings module

--- utils/greetings.py
+++ utils/greetings.py
@@ -3,6 +3,9 @@ from rich import print as rich_print
 
 
 def get_time_of_day():
+    """
+    This function returns a greeting based on the time of day.
+    """
     hour = datetime.now().hour
     if (hour >= 6) and (hour < 12):
         return "Good morning"
@@ -15,6 +18,16 @@ def get_time_of_day():
 
 
 def greet_user(USER, HOSTNAME):
+    """
+    A function that greets the user with the current time, username, and hostname.
+
+    Parameters:
+    - USER: a string representing the username
+    - HOSTNAME: a string representing the hostname
+
+    Return:
+    This function does not return anything.
+    """
     time_of_day = get_time_of_day()
     rich_print(
         f"{time_of_day} [bold magenta]{USER}[/bold magenta]. I am [bold magenta]{HOSTNAME}[/bold magenta]. How may I assist you?"
